ce7c4ebf6dc7c9a2ddd7c67aee74567b8611b85d
paulviet <9637489+paulviet@users.noreply.github.com>
2021-04-04 06:38:00 -0400

    Deliverable 2 - Mentorship Elegibility

--- QUERIES/Employee_Database_challenge.sql
+++ QUERIES/Employee_Database_challenge.sql
@@ -27,8 +27,27 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no ASC, t.to_date DESC-- 
 
 -- Retiring Titles
-SSELECT count(title), title
+SELECT count(title), title
 INTO retiring_titles
 FROM unique_titles
 group by title
 ORDER BY count(title) DESC
+
+-- Deliverable 2: Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title --,
+	--age(NOW(), e.birth_date) as current_age
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no) and (de.to_date = t.to_date)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') and de.to_date = '9999-01-01'
+-- 1549 rows affected.
+-- 1549 rows affected.
